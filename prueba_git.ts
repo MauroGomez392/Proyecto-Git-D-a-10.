dac344ccc474e13d34aabbe0e39af4f28b0ad259
MauroGomez392 <mauro.gomez.gad@gmail.com>
2020-10-19 20:30:23 +0200

    Esto ser para el Reto 3

---
+++ prueba_git.ts
@@ -0,0 +1,10 @@
+// Este es una prueba para Git
+// Esta es la segunda prueba para agregar el archivo y luego deshacer el added
+// A ver si logro sacarlo del added
+// qué pasaaaaa?
+// Esto está complicadiiittoooooo
+import { Book } from "../dia9 typescript/book";
+import {Book} from "./book";
+import {Library} from "./library";
+ 
+let mauro : Book = new Book ("Un día instalando GIT", 59432503947, "ISSN", "By Mauro", "By Mauro Mauro");
