4b2f58d2032a227bb9f28459bf4483ef84e46818
MauroGomez392 <mauro.gomez.gad@gmail.com>
2020-10-19 20:17:58 +0200

    Veamos si puedo des-commitearlo

---
+++ prueba_git.js
@@ -0,0 +1,5 @@
+// Este es una prueba para Git
+// Esta es la segunda prueba para agregar el archivo y luego deshacer el added
+// A ver si logro sacarlo del added
+// qué pasaaaaa?
+// Esto está complicadiiittoooooo
